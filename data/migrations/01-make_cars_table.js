d5c404f7fd4a357c9c26cfdf64d6aaecb7fabb45
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-05-17 17:55:04 -0700

    migration up

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,5 +1,28 @@
 exports.up = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.createTable("cars", tbl => {
+    tbl.increments() // creates automated id (in increments)
+
+    tbl.string("vin", 13) // constraints for that table w/ 13 digits
+    .notNullable()
+    .unique()
+
+    tbl.string("make", 50)
+    .notNullable()
+
+    tbl.string("model", 50)
+    .notNullable()
+
+    tbl.numeric("mileage")
+    .notNullable()
+    .unsigned()
+
+    tbl.string("title", 50)
+    .defaultTo("clean")
+
+    tbl.string("transmission", 50)
+    .defaultTo("automatic")
+  })
 };
 
 exports.down = function (knex) {
