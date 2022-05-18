1e3d22e15a70696dc5dd85966c734cdbfc95e032
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-05-18 11:04:11 -0700

    get cars

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,9 +1,16 @@
 const express = require("express")
 
+const Car = require("./cars-model")
+
 const router = express.Router()
 
 router.get("/", async (req, res, next) => {
-    res.json("get all cars")
+    try{
+        const cars = await Car.getAll()
+        res.json(cars)        
+    } catch(err) {
+        next(err)
+    }
 })
 
 router.get("/:id", async (req, res, next) => {
