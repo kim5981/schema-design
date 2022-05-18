cad0fac5061f1908675958a02c27259c437e5bd2
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-05-17 17:45:30 -0700

    seeds

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,38 @@
 // STRETCH
+const cars = [
+    {
+        vin: "0000000000000",
+        make: "G-Class",
+        model: "Mercedes-Benz",
+        mileage: 10000,
+        title: "clean",
+        transmission: "automatic"
+    },
+    {
+        vin: "0000000000001",
+        make: "Highlander",
+        model: "Toyota",
+        mileage: 50000,
+        title: "salvage"
+    },
+    {
+        vin: "0000000000002",
+        make: "A8",
+        model: "Audi",
+        mileage: 45000,
+        title: "clean",
+        transmission: "automatic"
+    },
+]
+
+// exports.seed = function(knex) {
+//     return knex("cars")
+//         .truncate().then(() => {
+//             return knex("cars").insert(cars) 
+//         })
+// }
+
+exports.seed = async function (knex) {
+    await knex("cars").truncate()
+    await knex("cars").insert(cars)
+}
