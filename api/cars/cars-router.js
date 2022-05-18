6a295b5c4baef569821fbf91c5eff0715c9a6475
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-05-18 10:50:16 -0700

    router structure

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,17 @@
-// DO YOUR MAGIC
+const express = require("express")
+
+const router = express.Router()
+
+router.get("/", async (req, res, next) => {
+    res.json("get all cars")
+})
+
+router.get("/:id", async (req, res, next) => {
+    res.json("get car with id")
+})
+
+router.post("/", async (req, res, next) => {
+    res.json("post car")
+})
+
+module.exports = router;
